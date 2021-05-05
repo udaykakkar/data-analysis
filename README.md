c70cc5ca82347cee1eae957394fd3cfeab32a222
Hsing-I <hsing.sophie@gmail.com>
2021-05-05 16:29:50 -0400

    scrape multiple pages

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ create virtual env: python3 -m venv venv
 activate: source venv/bin/activate
 
 start a scrapy project: scrapy startproject projectname
+
+result in a josn format: scrapy crawl indeed -o indeed.json  
--- job_scraper/job_scraper/spiders/indeed_spider.py
+++ job_scraper/job_scraper/spiders/indeed_spider.py
@@ -8,22 +8,19 @@ class IndeedSpider(scrapy.Spider):
     def parse(self, response):
         for job in response.css('div.jobsearch-SerpJobCard'):
             yield{
-                 'title' : job.css('a.jobtitle::attr(title)').get(),
-                 'company' : job.css('span.company::text').get(),
-                 'rating': job.css('span.ratingsContent::text').get(),
-                 'location' : job.css('div.location::text').get(),
-                 'salary' : job.css('span.salaryText::text').get(),
+                 'title' : job.css('h2.title a.jobtitle::attr(title)').get(),
+                 'company' : job.css('div.sjcl div span.company a::text').get(),
+                 'rating': job.css('div.sjcl div span.ratingsDisplay a span.ratingsContent::text').get(),
+                 'location' : job.css('div.recJobLoc::attr(data-rc-loc)').get(),
+                 'salary' : job.css('div.salarySnippet span.salary span.salaryText::text').get(),
                  'description': job.css('div.summary ul li::text').getall()
              }
 
-
         next_page = response.css('ul.pagination-list li a::attr(aria-label)').getall()
         next_page_link = response.css('ul.pagination-list li a::attr(href)').getall()
-        print("link", next_page_link)
 
-        # if next_page[-1].lower == "next":
-        #     yield response.follow(url=next_page_link[-1], callback=self.parse)        
-
-## scrapy crawl indeed -o indeed.json            
+        if next_page[-1].lower() == "next":
+            yield response.follow(url= 'https://ca.indeed.com' + next_page_link[-1], callback= self.parse)        
+           
 
 
