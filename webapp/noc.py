5aad3af2e81d0265c58d06eaf900ef16893694ec
Hsing-I <hsing.sophie@gmail.com>
2021-05-25 14:19:08 -0400

    noc csv to json

--- webapp/noc.py
+++ webapp/noc.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import json
 
 df = pd.read_csv('noc-cnp.csv')
-df.groupby('Code',  sort=True)
+nocdf = df.groupby('Code',  sort=True,  as_index=False).agg(
+    {'Element Description English': lambda x: list(x)})  
+nocdf = nocdf.rename({'Code': 'code', 'Element Description English': 'job_titles'}, axis=1)
+noc_json = nocdf.to_json(orient='records')
+noc_parsed= json.loads(noc_json)
 
-print("data frame", df)
+with open('noc.json', 'w') as file:
+    json.dump(noc_parsed, file, indent=4)
